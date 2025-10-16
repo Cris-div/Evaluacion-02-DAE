be33f9d4048129eaa3911a3b2b520df60259e58e
Cris-div <christian.unocc@tecsup.edu.pe>
2025-10-15 22:38:03 -0500

    view

--- foodorder/views.py
+++ foodorder/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from .models import Plato, Pedido
+from .serializers import PlatoSerializer, PedidoSerializer
+
+
+# --- Raíz de la API ---
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'message': '🍔 Welcome to FoodOrder API',
+        'version': 'v1.0',
+        'description': 'Sistema de gestión de platos y pedidos para restaurante',
+        'endpoints': {
+            'platos': reverse('plato-list', request=request, format=format),
+            'pedidos': reverse('pedido-list', request=request, format=format),
+        }
+    })
+
+
+class PlatoViewSet(viewsets.ModelViewSet):
+    queryset = Plato.objects.all()
+    serializer_class = PlatoSerializer
+
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer

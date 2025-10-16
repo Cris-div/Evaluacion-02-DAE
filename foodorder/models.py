ca0472be3fa6c9cf4c6634820212f811db0ad85e
Cris-div <christian.unocc@tecsup.edu.pe>
2025-10-15 22:23:41 -0500

    models

--- foodorder/models.py
+++ foodorder/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Plato(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+class Pedido(models.Model):
+    Estados=[('pendiente','Pendiente'),
+            ('en_proceso','En Proceso'),
+            ('entregado','Entregado'),
+            ('cancelado','Cancelado')]
+    fecha = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    estado = models.CharField(max_length=20 ,choices=Estados , default='pendiente')
+
+    platos= models.ManyToManyField(Plato, related_name='pedidos')
+    def __str__(self):
+        return self.estado
